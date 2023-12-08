151b9004a04d6ee1e348c2078f7fdd2f86fc1f22
victorbgh16 <victor.berghausen@gmail.com>
2023-12-08 11:35:44 +0100

    finished day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -41,3 +41,35 @@ for e in games:
     #     total += int(id)
 
 print(total)
+
+total = 0
+
+for e in games:
+    id = e.split(':')[0]
+    id = id.split()[1]
+    values = e.split(':')[1].strip()
+    values = values.split(';')
+
+    max_red = 0
+    max_green = 0
+    max_blue = 0
+
+    for elem in values:
+        no_cubes = elem.split(',')
+
+        for item in no_cubes:
+            item = item.strip()
+
+            num = int(item.split()[0])
+            color = item.split()[1]
+
+            if num > max_red and color == 'red':
+                max_red = num
+            elif num > max_green and color == 'green':
+                max_green = num
+            elif num > max_blue and color == 'blue':
+                max_blue = num
+
+    total += max_red * max_green * max_blue
+
+print(total)
